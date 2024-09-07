5c4d61babd0abfd1625fe52d7c5190d64240825a
ravimathuriya <166046845+ravimathuriya@users.noreply.github.com>
2024-09-07 20:59:37 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongourl = "mongodb+srv://ravimathuriya777:Ravi@1507@cluster0.vpw1h.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
+const mongourl = "mongodb+srv://ravimathuriya777:Ravi@1507@cluster0.vpw1h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 const connecttomongoose = () =>{
