331b6d1afbf1b3f8411e90c0ef0af0d032ef2f4b
ravimathuriya <166046845+ravimathuriya@users.noreply.github.com>
2024-09-08 11:06:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://myfullstackportfolio-front.vercel.app"],
         methods: ["POST", "GET"],
         credentials:true         
               }
