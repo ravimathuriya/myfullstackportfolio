3191d43db6307c09c47124139739a005429c9c6f
ravimathuriya <166046845+ravimathuriya@users.noreply.github.com>
2025-04-28 20:53:15 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,7 @@ import React from "react";
 import Body from "./Body";
 
 
-
 const About = () => {
-
 
   return (
     <>
@@ -15,14 +13,14 @@ const About = () => {
             <h2>About Me</h2>
             <hr />
             <h3 >Intro</h3>
-            <p>Hi, My Name is Ravi Mathuriya. I belong to Aligarh city which is situated in Uttar Pradesh. I am an Electrical Engineer. I’m passionate about today’s topic because, I am aspiring frontend develpoer. I want to work in IT Sector because of my passion to learn and to work in the IT Sector. Recentely, I have  skilled in ReactJs and front-end development. Now, I am learning MERN stack development to became a full stack developer. I am fresher in IT sector but I have experienced 5+ years in various corporate sector. </p>
+            <p>Hi, My name is Ravi Mathuriya, and I am from Aligarh, Uttar Pradesh. I hold a background in Electrical Engineering but have transitioned into the world of Web Development. With over 2 years of hands-on experience in the MERN stack (MongoDB, Express.js, React, Node.js), I’ve found my true passion in creating dynamic, scalable, and user-focused web applications. After spending 5 years in the electrical field, I decided to follow my interest in technology and software development, and now I am fully committed to advancing my career in web development. I am excited to contribute my skills and continue growing in a MERN stack-focused role, building innovative solutions and applications.</p>
             <h3>I Like</h3>
             
               <ul>
                 <li>Playing Cricket</li>
                 <li>Keep Tracking Share Market</li>
                 <li>Watching Movies</li>
-                <li>Listing Movies</li>
+                <li>Listning Music</li>
                 <li>Travelling New Cities</li>
               </ul>
 
@@ -45,3 +43,5 @@ const About = () => {
 };
 
 export default About;
+
+
