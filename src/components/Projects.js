513204dd0792cd72547b6c3d957f9a4036377e59
ravimathuriya <166046845+ravimathuriya@users.noreply.github.com>
2025-04-28 20:56:51 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,106 +2,68 @@ import React from "react";
 import Body from "./Body";
 import ProjectCard from "./ProjectCard";
 
-
 const Projects = () => {
-
   const projects = [
     {
-      title: "Quiz App",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/tTws32B1/quiz.png",
-      gitURL: "https://ravimathuriya.github.io/quizapp/"
-    },
-  {
-      title: "qr code generator",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/9QDZ7wmN/qr.png",
-      gitURL: "https://ravimathuriya.github.io/qrcodegenerator/"
-    },
-  {
-      title: "Lyrics Finder",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/cJnsw08S/ly.png",
-      gitURL: "https://ravimathuriya.github.io/lyricsfinder/"
-    },
-  {
-      title: "Rock Paper Scissor",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/TY35m52v/rock.png",
-      gitURL: "https://ravimathuriya.github.io/rockpaperscissor/"
-    },
-  {
-      title: "Coin Flipping",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/KYRdgNw3/co.png",
-      gitURL: "https://ravimathuriya.github.io/flipthecoin/"
-    },
-  {
-      title: "Domain Name Finder",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/7YS0KpGM/dom.png",
-      gitURL: "https://ravimathuriya.github.io/domainnamefinder/"
-    },
-  {
-      title: "Text Converter",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/rpHNB58S/conver.png",
-      gitURL: "https://ravimathuriya.github.io/home"
-    },
-  {
-      title: "Avatar Generator",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/0NvspJg0/avatar.png",
-      gitURL: "https://ravimathuriya.github.io/avatargenerator/"
+      title: "Personal Expense Tracker",
+      description:
+        "The Personal Expense Tracker is a full-stack web application built using the MERN stack (MongoDB, Express.js, React, Node.js) that allows users to track their personal expenses securely. The app enables users to log in, add, edit, update, and delete their expense entries with ease, providing a seamless experience for managing finances.",
+      imageURL: "https://i.postimg.cc/wTBWsv20/Expense-Tracker.png",
+      gitURL: "https://personal-expense-tracker-frontend-phi.vercel.app/",
     },
     {
-      title: "Movie Mania",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/tTX7kkxZ/Screenshot-4.png",
-      gitURL: "https://ravimathuriya.github.io/movieapp/"
+      title: "Blogging platform",
+      description:
+        "The Blogging Platform is a full-stack web application built using the MERN stack (MongoDB, Express.js, React, Node.js), designed to allow users to log in, create, edit, update, and delete their blog posts. The platform provides a user-friendly interface for bloggers to manage their content and interact with the community.",
+      imageURL: "https://i.postimg.cc/SQgdq448/Blog-Platform.png",
+      gitURL: "https://blog-platform-frontend-2x10.onrender.com/",
     },
-    {
-      title: "Dictionary Application",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/502MJb2W/Screenshot-5.png",
-      gitURL: "https://ravimathuriya.github.io/dictionaryapplication/"
-    },
-    {
-      title: "Currency Converter",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/Bb8DP74F/cur.png",
-      gitURL: "https://ravimathuriya.github.io/currencyconverter/"
-    },
-    {
-      title: "US Intraday Charts",
-      description: "to be updated",
-      imageURL: "https://i.postimg.cc/L40Xckpp/Screenshot-6.png",
-      gitURL: "https://ravimathuriya.github.io/usintradaycharts/"
-    },
-  ]
 
-  
+    {
+      title: "Quiz App",
+      description:
+        "The Quiz App is a web application designed to provide users with an interactive quiz experience. Built using React, it allows users to answer multiple-choice questions and see their results in real-time. The app dynamically loads questions and tracks the user's score, providing feedback at the end of the quiz.",
+      imageURL: "https://i.postimg.cc/tTws32B1/quiz.png",
+      gitURL: "https://ravimathuriya.github.io/quizapp/",
+    },
+
+    {
+      title: "Avatar Generator",
+      description:
+        "The Avatar Generator App is a dynamic web application built using React that allows users to generate random avatars with the help of an external API. By clicking a button, users can generate and display a unique avatar image based on predefined characteristics",
+      imageURL: "https://i.postimg.cc/0NvspJg0/avatar.png",
+      gitURL: "https://ravimathuriya.github.io/avatargenerator/",
+    },
+  ];
 
   return (
-    < >
-    
-      <div className='container-fluid' >
-        <div className="row justify-content-md-center" id='reverse'>
+    <>
+      <div className="container-fluid">
+        <div className="row justify-content-md-center" id="reverse">
           <Body />
-          <div className='col border mx-5 my-5' id='contact'>
+          <div className="col border mx-5 my-5" id="contact">
             <div id="contact-sizing">
               <h2>Projects </h2>
               <hr />
-              { projects.map((project, index)=>{
-                return (<ProjectCard key={index} title={project.title} description={project.description} imageURL={project.imageURL} gitURL={project.gitURL}  />)
-              }) }
+              {projects.map((project, index) => {
+                return (
+                  <ProjectCard
+                    key={index}
+                    title={project.title}
+                    description={project.description}
+                    imageURL={project.imageURL}
+                    gitURL={project.gitURL}
+                  />
+                );
+              })}
             </div>
           </div>
         </div>
-     </div>
-
+      </div>
     </>
   );
 };
 
 export default Projects;
+
+
